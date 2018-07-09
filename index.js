963cae4bb3625494aa0c4474c35142944936f5e2
Anandkumar <anandkumar316sw@gmail.com>
2018-07-09 17:29:49 +0530

    Assignment-2 Solution

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  const uniqueArray = [];
+  for (let i = 0; i < items.length; i += 1) {
+    if (uniqueArray.indexOf(items[i]) === -1) {
+      uniqueArray.push(items[i]);
+    }
+  }
+  return uniqueArray;
+};
 
 module.exports = removeDuplicateItems;
